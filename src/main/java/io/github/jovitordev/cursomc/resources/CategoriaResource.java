8745ede56e4dc68c8c3f23bc1788bbfcfe77a43a
jovitordev <vitortraww@gmail.com>
2020-07-16 11:11:28 -0300

    testando a primeira classe de dominio - categoria

--- src/main/java/io/github/jovitordev/cursomc/resources/CategoriaResource.java
+++ src/main/java/io/github/jovitordev/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package io.github.jovitordev.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.jovitordev.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST ESTÁ FUNCIONANDO!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
